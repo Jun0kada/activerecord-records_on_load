3a73b0211d242c5e3ed8e96cd79a6d2b88e629bc
Jun0kada <jun.okada0203@gmail.com>
2019-03-01 20:48:43 +0900

    Accept multi on load blocks

--- lib/active_record/records_on_load.rb
+++ lib/active_record/records_on_load.rb
@@ -2,12 +2,17 @@ module ActiveRecord
   module RecordsOnLoad
     def load(&block)
       super(&block)
-      @_on_load_block.call(@records) if @_on_load_block
+
+      if @_on_load_blocks.present?
+        @_on_load_blocks.each { |b| b.call(@records) }
+      end
+
       self
     end
 
     def on_load(&block)
-      @_on_load_block = block
+      @_on_load_blocks ||= []
+      @_on_load_blocks << block
       self
     end
   end
--- spec/activerecord-records_on_load_spec.rb
+++ spec/activerecord-records_on_load_spec.rb
@@ -3,17 +3,50 @@ require 'spec_helper'
 describe 'ActiveRecord::Relation#on_load' do
   context 'when loaded records' do
     it 'should hooked on_load' do
-      hooked = false
-      User.all.on_load { hooked = true }.load
-      expect(hooked).to eq(true)
+      hooked_count = 0
+      User.all.on_load { hooked_count += 1 }.load
+      expect(hooked_count).to eq(1)
+    end
+
+    it 'should hooked multi on_load' do
+      hooked_count = 0
+      User.all.on_load {
+        hooked_count += 1
+      }.on_load {
+        hooked_count += 1
+      }.load
+      expect(hooked_count).to eq(2)
+    end
+
+    it 'should get records' do
+      User.delete_all
+      user = User.create(name: 'John')
+      record_size = 0
+      record_ids = []
+      User.all.on_load { |records|
+        record_size = records.size
+        record_ids = records.map(&:id)
+      }.load
+      expect(record_size).to eq(1)
+      expect(record_ids).to eq([user.id])
     end
   end
 
   context 'when not loaded records' do
     it 'should not hooked on_load' do
-      hooked = false
-      User.all.on_load { hooked = true }
-      expect(hooked).to eq(false)
+      hooked_count = 0
+      User.all.on_load { hooked_count += 1 }
+      expect(hooked_count).to eq(0)
+    end
+
+    it 'should not hooked multi on_load' do
+      hooked_count = 0
+      User.all.on_load {
+        hooked_count += 1
+      }.on_load {
+        hooked_count += 1
+      }
+      expect(hooked_count).to eq(0)
     end
   end
 
